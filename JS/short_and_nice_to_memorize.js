20d9155608efa8745bb6f197ead57dbef1c81b0b
Aleksandr <said46@yandex.ru>
2025-10-15 23:34:05 +1100

    Update short_and_nice_to_memorize.js

--- JS/short_and_nice_to_memorize.js
+++ JS/short_and_nice_to_memorize.js
@@ -1,1 +1,13 @@
 events = $._data($('#OverrideMethodId')[0], 'events');
+
+// Find the Correct URL by Monitoring Network
+// First, open browser Network tab, then trigger a type change manually
+// Then run this to see what URLs are being called:
+
+// Monitor all AJAX requests temporarily
+$(document).ajaxComplete(function(event, xhr, settings) {
+    console.log("🔍 AJAX Call:", settings.url, "Method:", settings.type);
+    if (settings.type === 'GET' && settings.url.includes('Override')) {
+        console.log("📌 Possible cascade URL:", settings.url);
+    }
+});
